a1ae4c0878f82ce571d083f5eb5d206b667bbda4
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-30 16:57:31 -0700

    Pre-User Details

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,68 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/registration_form", methods=["GET", "POST"])
+def register_process():
+	"""Registration form and process"""
+
+	if request.method == "GET":
+		return render_template("registration_form.html")
+
+	else:
+		email = request.form.get("email")
+		password = request.form.get("password")
+
+		if not User.query.filter(User.email == email).first():
+			user = User(email=email, password=password)
+			db.session.add(user)
+			db.session.commit()
+			return redirect("/")
+		else:
+			flash('That e-mail already exists!')
+			return render_template("registration_form.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	"""Log in."""
+
+	if request.method == "GET":
+		return render_template("login.html")
+
+	else:
+		email = request.form.get("email")
+		password = request.form.get("password")	
+
+		q = User.query
+
+		if q.filter((User.email == email), (User.password == password)).first():
+			session["logged_in_user"] = q.filter(User.email == email).one().user_id
+
+			flash("Logged in!")
+			return redirect("/")
+		else:
+			flash("The e-mail or password is incorrect.")
+			return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+	"""Log out."""
+
+	del session["logged_in_user"]
+	flash("Successfully logged out!")
+	return redirect("/")
 
 
 if __name__ == "__main__":
@@ -36,5 +97,6 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     app.run(port=5000, host='0.0.0.0')
