13d84ca616f04de332b46c7971c73bcb558759a9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-29 17:14:29 -0700

    Finished pre-further study lab

--- seed.py
+++ seed.py
@@ -94,6 +94,19 @@ def set_val_user_id():
     db.session.commit()
 
 
+def set_val_movie_id():
+    """Set value for the next movie_id after seeding database"""
+
+    # Get the Max movie_id in the database
+    result = db.session.query(func.max(Movie.movie_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next movie_id to be max_id + 1
+    query = "SELECT setval('movies_movie_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
 if __name__ == "__main__":
     connect_to_db(app)
 
@@ -105,3 +118,4 @@ if __name__ == "__main__":
     load_movies()
     load_ratings()
     set_val_user_id()
+    set_val_movie_id()
